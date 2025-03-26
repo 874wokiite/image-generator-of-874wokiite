a7b812174586b2903e7087046710f4c6096cdfe7
tosuri13 <85057870+tosuri13@users.noreply.github.com>
2025-03-26 23:43:31 +0900

    :recycle: コードを綺麗にして、CORSヘッダーを追加した

--- server/src/function.py
+++ server/src/function.py
@@ -1,55 +1,64 @@
-import base64
 import json
 import os
-import time
+from base64 import b64decode
+from uuid import uuid4
 
 import boto3
 from openai import OpenAI
+
+OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
 
 
 def handler(event, context):
     request = json.loads(event["body"])
 
-    client = OpenAI()
-    client.api_key = os.getenv("OPENAI_API_KEY")  # API keyのセット
+    # OpenAIのDALL・E 3を使用して画像を生成する
+    client = OpenAI(api_key=OPENAI_API_KEY)
     response = client.images.generate(
-        model="dall-e-3",  # モデル
-        prompt=request["text"],  # プロンプト
-        n=1,  # 生成数
-        size="1024x1024",  # 解像度 dall-e-3では1024x1024、1792x1024、1024x1792
-        response_format="b64_json",  # レスポンスフォーマット url or b64_json
-        quality="hd",  # 品質 standard or hd
-        style="vivid",  # スタイル vivid or natural
+        model="dall-e-3",
+        prompt=request["text"],
+        n=1,
+        size="1024x1024",
+        response_format="b64_json",
+        quality="standard",
+        style="vivid",
     )
 
-    # S3クライアントの作成
-    s3_client = boto3.client("s3")
+    # 画像をLambdaのエフェメラルストレージに保存
+    file_name = f"{str(uuid4())}.png"
+    with open(f"/tmp/{file_name}", "wb") as file:
+        file.write(b64decode(str(response.data[0].b64_json)))
+
+    s3_client = boto3.client("s3", region_name="ap-northeast-1")
     bucket_name = "image-generator-of-874wokiite-bucket"
 
-    # 画像保存
-    for d in response.data:
-        file_name = f"{int(time.time())}.png"
-        with open(f"/tmp/{file_name}", "wb") as f:
-            f.write(base64.b64decode(d.b64_json))
-
-    # S3にアップロード
-    s3_client.upload_file(f"/tmp/{file_name}", bucket_name, file_name)
-
-    # 署名付きURLを取得
-    presigned_url = s3_client.generate_presigned_url(
-        "get_object", Params={"Bucket": bucket_name, "Key": file_name}, ExpiresIn=3600
+    # S3に生成した画像をアップロード
+    s3_client.upload_file(
+        Filename=f"/tmp/{file_name}",
+        Bucket=bucket_name,
+        Key=file_name,
     )
 
-    # 署名付きURLを返す
+    # アップロードした画像の署名付きURLを取得
+    presigned_url = s3_client.generate_presigned_url(
+        ClientMethod="get_object",
+        Params={
+            "Bucket": bucket_name,
+            "Key": file_name,
+        },
+        ExpiresIn=3600,
+    )
+
     return {
         "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "Authorization,Content-Type",
+            "Access-Control-Allow-Methods": "POST,OPTIONS",
+        },
         "body": json.dumps(
             {
                 "presigned_url": presigned_url,
             }
         ),
     }
-
-
-if __name__ == "__main__":
-    handler(None, None)
